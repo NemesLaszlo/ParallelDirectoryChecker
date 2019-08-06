2b6eb2d1775d5833e1066567e75461fb93b15e69
NemesLaszlo <wow.laszlo@gmail.com>
2019-08-06 23:22:44 +0200

    Parallel directory implementation

---
+++ ParallelDirectoryChecking/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// Iterate File Directories with the Parallel Class
+/// 
+/// The following example iterates the directories sequentially, but processes the files in parallel. This is
+/// probably the best approach when you have a large file-to-directory ratio. It is also possible to 
+/// parallelize the directory iteration, and access each file sequentially. It is probably not efficient to
+/// parallelize both loops unless you are specifically targeting a machine with a large number of
+/// processors. However, as in all cases, you should test your application thoroughly to determine
+/// the best approach.
+/// </summary>
+namespace ParallelDirectoryChecking
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TraverseTreeParallelForEach(@"C:\Program Files", (f) =>
+                {
+                    // Exceptions are no-ops.
+                    try
+                    {
+                        // Do nothing with the data except read it.
+                        byte[] data = File.ReadAllBytes(f);
+                    }
+                    catch (FileNotFoundException) { }
+                    catch (IOException) { }
+                    catch (UnauthorizedAccessException) { }
+                    catch (SecurityException) { }
+                    // Display the filename.
+                    Console.WriteLine(f);
+                });
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine(@"The directory 'C:\Program Files' does not exist.");
+            }
+
+            // Keep the console window open.
+            Console.ReadKey();
+        }
+
+        public static void TraverseTreeParallelForEach(string root, Action<string> action)
+        {
+            //Count of files traversed and timer for diagnostic output
+            int fileCount = 0;
+            var sw = Stopwatch.StartNew();
+
+            // Determine whether to parallelize file processing on each folder based on processor count.
+            int procCount = System.Environment.ProcessorCount;
+
+            // Data structure to hold names of subfolders to be examined for files.
+            Stack<string> dirs = new Stack<string>();
+
+            if (!Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs = { };
+                string[] files = { };
+
+                try
+                {
+                    subDirs = Directory.GetDirectories(currentDir);
+                }
+                // Thrown if we do not have discovery permission on the directory.
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                // Thrown if another process has deleted the directory after we retrieved its name.
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                try
+                {
+                    files = Directory.GetFiles(currentDir);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                // Execute in parallel if there are enough files in the directory.
+                // Otherwise, execute sequentially.Files are opened and processed
+                // synchronously but this could be modified to perform async I/O.
+                try
+                {
+                    if (files.Length < procCount)
+                    {
+                        foreach (var file in files)
+                        {
+                            action(file);
+                            fileCount++;
+                        }
+                    }
+                    else
+                    {
+                        Parallel.ForEach(files, () => 0, (file, loopState, localCount) =>
+                        {
+                            action(file);
+                            return (int)++localCount;
+                        },
+                                         (c) => {
+                                             Interlocked.Add(ref fileCount, c);
+                                         });
+                    }
+                }
+                catch (AggregateException ae)
+                {
+                    ae.Handle((ex) => {
+                        if (ex is UnauthorizedAccessException)
+                        {
+                            // Here we just output a message and go on.
+                            Console.WriteLine(ex.Message);
+                            return true;
+                        }
+                        // Handle other exceptions here if necessary...
+
+                        return false;
+                    });
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+
+            // For diagnostic purposes.
+            Console.WriteLine("Processed {0} files in {1} milliseconds", fileCount, sw.ElapsedMilliseconds);
+        }
+    }
+}
